7a59c2ba23f7d39590a7bd4131179a7c27e36315
azeuusa <reza.fahrezawe@gmail.com>
2026-07-19 13:31:20 +0700

    p balap

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use std::process::Command;
+use std::io;
+use rand::Rng;
+
+
+fn main() {
+
+  let angka = rand::thread_rng().gen_range(1..=10);
+
+  loop {
+      
+  
+  println!("tebak angka 1 -> 10");
+
+  let mut input = String::new();
+  
+  
+
+  io::stdin()
+  .read_line(&mut input)
+  .expect("gagal read jit");
+
+  let tebakan: u32 = match input.trim().parse() {
+
+    Ok(anga) => anga,
+    Err(_) => continue
+  };
+
+
+  
+
+  if tebakan == angka {
+
+    println!("kamu bener");
+      
+  }
+
+  else {
+
+    Command::new("sh")
+    .arg("-c")
+    .arg("echo 'hello world'")
+    .output()
+    .expect("kanyut");
+    break;
+  }
+
+  
+
+
+
+  
+    
+  
+
+   }
+
+    
+}
